172ef0679bf45ca2186573b8b07d25fa2dbaa222
mmpau <mariaprggc@gmail.com>
2025-04-01 09:35:10 -0500

    Add files via upload

---
+++ Corte1/loops/Graficos Maria.py
@@ -0,0 +1,6 @@
+import random #Nmueros aleatorios
+from matplotlib import pyplot as plt
+numerosx= range[1, 13]
+numerosy= [random.randint(1, 2000) for i in range(12)]
+plt.plot(numerosx, numerosy)
+plt.show()
