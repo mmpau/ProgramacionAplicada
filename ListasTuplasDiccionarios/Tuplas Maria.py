e4f2d8219c9a060f579a252e9c58ca1083347c09
mmpau <mariaprggc@gmail.com>
2025-04-01 09:16:44 -0500

    Tuplas Maria.py

--- ListasTuplasDiccionarios/Tuplas Maria.py
+++ ListasTuplasDiccionarios/Tuplas Maria.py
@@ -9,14 +9,14 @@ my_lista.append("Pollo")
 print(my_lista)
 my_lista.insert(3 , "Casa")
 print(my_lista)
-my_lista.extend("Comida ", "Universidad ")
+my_lista.extend(["Universidad"])#Se pon en llaves para que lleve toda la palabra, no letra por letra
 print(my_lista)
 print(my_lista.index("Carro"))
 my_lista.remove("Esposa")
 print(my_lista)
 my_lista.insert(8 , "Marron")
 print(my_lista)
-print(my_lista.pop)
+print(my_lista.pop())
 
 Tamaño= len(my_lista)
 print("El tamaño de la lista es:", Tamaño)
@@ -25,14 +25,14 @@ print("La lista se repetira 3 veces:" , my_lista3)
 
 
 print("Ordenamiento de listas")
-print("Orden alfabetico de la lista: ")
-my_listasort=my_lista.sort
-print(my_listasort)
+print("Orden alfabético de la lista:")
+my_lista.sort()  
+print(my_lista)
 
-Listanum=[1 , 33 ,44 ,56 , 87, 46, 123]
-print("Ordenar números de menor a mayor: ")
-Listanumsort= Listanum.sort
-print(Listanumsort)
+Listanum = [1, 33, 44, 56, 87, 46, 123]
+print("Ordenar números de menor a mayor:")
+Listanum.sort()
+print(Listanum)
 
 
 Listanum.sort(reverse=True)
@@ -46,3 +46,20 @@ print("Mi tupla es ", mitupla)
 print(mitupla[0])
 
 print(mitupla[2])
+
+print("carro" in mitupla)
+print(mitupla.count("Carro")) #Cuenta cuantas veces esta la palabra
+
+Tuplaunitaria=("Blanco",)
+print(Tuplaunitaria)
+
+mitupla= "Maria" , 11 , 11 , 2005
+print(mitupla)
+
+nombre, dia, mes , año = mitupla
+
+print(nombre)
+print(dia)
+print(mes)
+print(año)
+
